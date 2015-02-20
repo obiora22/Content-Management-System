0fcbb21e7eaaa8838a3571d9b8384b9dc1814fd3
Obi Anaedozie <dozie22@gmail.com>
2015-02-20 02:07:19 -0500

    Add controllers and associated views for Subject and Page Models

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,52 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      flash[:notice] = "Your page was created successfully."
+      redirect_to(:action =>'index')
+    else
+      render('new')  
+    end
+  end
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      flash[:notice] = "Your page was updated successfully."
+       redirect_to(:action => 'show',:id => @page.id)
+    else
+       render('edit')
+       end   
+  end
+
+  def delete
+    @page = Page.find(params[:id])
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    flash[:notice] = "Subject '#{@page.name}' was destroyed successfully."
+    redirect_to(:action => 'index', :notice => "Page was successfully deleted.")
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:name, :permalink, :position, :visible)
+  end
+end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -13,23 +13,44 @@ class SubjectsController < ApplicationController
   def new
     @subject = Subject.new({:name => "Default"})
   end
+
   def create
     @subject = Subject.new(subject_params)
     if @subject.save
+      flash[:notice] = "Subject created successfully."
       redirect_to(:action => 'index')
     else
       render(:action => 'new')
+    end
+  end
 
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+    # if @subject.save
+      flash[:notice] = "Subject '#{subject.name}' was updated successfully."
+      redirect_to(:action => 'show',:id => @subject.id)
+    else
+      render(:action => 'new')
     end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def delete
+     @subject = Subject.find(params[:id])
   end
+  def destroy
+      subject = Subject.find(params[:id])
+      subject.destroy
+       flash[:notice] = "Subject '#{subject.name}' was destroyed successfully."
+      redirect_to(:action => 'index', :notice => "Item was successfully deleted.")
+  end
+
   private
-  
+
   def subject_params
     params.require(:subject).permit(:name, :position, :visible)
   end
